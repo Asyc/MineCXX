56ab6e0b653d6bf4b778c06a9d49c41a1556ea44
Asyc <ali@asyc.me>
2021-07-25 23:39:52 -0500

    Rearrange Main

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -13,102 +13,109 @@ struct Vertex {
     float tx, ty;
 };
 
+std::array<Vertex, 4> vertices{
+        Vertex{-0.5f, 0.5f, 0.1f, 0.0f, 1.0f},
+        Vertex{-0.5f, -0.5f, 0.1f, 0.0f, 0.0f},
+        Vertex{0.5f, -0.5f, 0.1f, 1.0f, 0.0f},
+        Vertex{0.5f, 0.5f, 0.1f, 1.0f, 1.0f}
+};
+
+std::array<uint32_t, 6> indices{ 0, 1, 2, 0, 3, 2 };
+
 template<typename T>
 constexpr size_t size_of(size_t val) {
     return sizeof(T) * val;
 }
 
-void app_main() {
-    engine::init();
+void tickFps();
+const std::u16string& getFpsString();
 
+void app_main() {
     Window window(1920, 1080, "Window");
 
-    auto context = window.createRenderContext(Swapchain::SwapchainMode::DOUBLE_BUFFER_VSYNC);
+    auto context = window.createRenderContext(Swapchain::SwapchainMode::DOUBLE_BUFFER);
     auto pipeline = context->createRenderPipeline(engine::File("assets/shaders/basic"));
     auto pool = context->createCommandPool();
-
-    std::array<Vertex, 4> vertices{
-        Vertex{-0.5f, 0.5f, 0.1f, 0.0f, 1.0f},
-        Vertex{-0.5f, -0.5f, 0.1f, 0.0f, 0.0f},
-        Vertex{0.5f, -0.5f, 0.1f, 1.0f, 0.0f},
-        Vertex{0.5f, 0.5f, 0.1f, 1.0f, 1.0f}
-    };
-    std::array<uint32_t, 6> indices{0, 1, 2, 0, 3, 2};
-
+    
     auto vertexBuffer = context->allocateVertexBuffer(size_of<Vertex>(vertices.size()));
-    vertexBuffer->write(0, vertices.data(), size_of<Vertex>(vertices.size()));
-
     auto indexBuffer = context->allocateIndexBuffer(indices.size());
+    vertexBuffer->write(0, vertices.data(), size_of<Vertex>(vertices.size()));
     indexBuffer->write(0, indices.data(), indices.size());
 
     auto texture = context->createImage(engine::File(R"(C:\Users\Asyc\Downloads\hippo.jpg)"));
-
     auto uniformDescriptorSet = pipeline->allocateDescriptorSet(0);
     uniformDescriptorSet->bind(0, *texture);
 
-    auto render = pool->allocateCommandListImmutable();
-    render->begin();
-    render->bindPipeline(*pipeline);
-    render->bindUniformDescriptor(*uniformDescriptorSet);
-    render->bindVertexBuffer(*vertexBuffer);
-    render->bindIndexBuffer(*indexBuffer);
-    render->drawIndexed(1, indices.size());
-    render->end();
-
-    auto fontBuffer = pool->allocateDirectCommandBuffer();
-
-    auto buffer = pool->allocateIndirectCommandBuffer();
+    auto quad = pool->allocateDirectCommandBuffer();
+    auto font = pool->allocateDirectCommandBuffer();
 
     size_t fps = 0;
-    std::u16string stringFps = u"FPS: 0000";
+    std::u16string stringFps ;
 
     auto last = std::chrono::high_resolution_clock::now();
 
     while (!window.shouldClose()) {
         Window::pollEvents();
 
-        fontBuffer->begin();
-        context->getFontRenderer().draw(*fontBuffer, stringFps);
-        fontBuffer->end();
+        // Record Command Buffers
+        quad->begin();
+        quad->bindPipeline(*pipeline);
+        quad->bindUniformDescriptor(*uniformDescriptorSet);
+        quad->bindVertexBuffer(*vertexBuffer);
+        quad->bindIndexBuffer(*indexBuffer);
+        quad->drawIndexed(1, indices.size());
+        quad->end();
 
-        buffer->begin();
-        buffer->accept(*render);
-        buffer->end();
+        font->begin();
+        context->getFontRenderer().draw(*font, getFpsString());
+        font->end();
 
-        context->getSwapchain().submitCommandBuffer(*buffer);
-        context->getSwapchain().submitCommandBuffer(*fontBuffer);
-
+        // Submission
+        context->getSwapchain().submitCommandBuffer(*quad);
+        context->getSwapchain().submitCommandBuffer(*font);
         context->getSwapchain().nextImage();
 
-        auto now = std::chrono::high_resolution_clock::now();
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(now - last).count() >= 1000) {
-            last = now;
-
-            std::string numeric = std::to_string(fps);
-
-            size_t index = 0;
-            for (char i : numeric) {
-                stringFps[index + 5] = (char16_t) i;
-                index++;
-            }
-
-            stringFps[index + 5] = '\0';
-
-            fps = 0;
-        } else {
-            fps++;
-        }
+        tickFps();
     }
-
-    engine::terminate();
 }
+
+std::chrono::high_resolution_clock::time_point lastTick = std::chrono::high_resolution_clock::now();
+std::u16string fpsString = u"FPS: 0000";
+size_t fpsCounter = 0;
+
+void tickFps() {
+    auto now = std::chrono::high_resolution_clock::now();
+    if (std::chrono::duration_cast<std::chrono::seconds>(now - lastTick).count() >= 1) {
+        std::string numeric = std::to_string(fpsCounter);
+
+        for (size_t i = 0; i < numeric.size(); i++) {
+            fpsString[i + 5] = static_cast<char16_t>(numeric[i]);
+        }
+
+        fpsString[numeric.size() + 5] = u'\0';
+
+        fpsCounter = 0;
+        lastTick = now;
+    } else {
+        fpsCounter++;
+    }
+}
+
+const std::u16string& getFpsString() {
+    return fpsString;
+}
+
 
 int main() {
 #ifdef MCE_DBG
+    engine::init();
     app_main();
+    engine::terminate();
 #else
     try {
+        engine::init();
         app_main();
+        engine::terminate();
         return 0;
     } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
